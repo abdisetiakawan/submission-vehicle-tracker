6e554fcac79b3892318617a5878d867fa11d7690
abdisetiakawan <bedikadiryt@gmail.com>
2025-07-25 00:25:39 +0700

    chore: Updates CORS origin to production URL
    Updates the CORS origin to the production URL to allow requests from the deployed application.

--- vehicle-tracker-server/src/server.ts
+++ vehicle-tracker-server/src/server.ts
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "https://submission-vehicle-tracker.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
